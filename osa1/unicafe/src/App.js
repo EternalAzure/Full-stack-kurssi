54265db1781a6e7c84442177e970d66ff99582d3
Miika Kalliokoski <kall.miika@outlook.com>
2021-04-16 15:16:35 +0300

    Korjattu varoitus lisäämällä <tbody>

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -45,12 +45,14 @@ const Button = ({ handleClick, text }) => (
 const Statistics = ({good, neutral, bad}) => {
   return(
     <table>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={good+neutral+bad} />
-      <StatisticLine text="average" value={parseFloat((good-bad)/(good+neutral+bad)).toFixed(4)} />
-      <StatisticLine text="positive" value={parseFloat(good/(good+neutral+bad)*100).toFixed(2)+"%"} />
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={good+neutral+bad} />
+        <StatisticLine text="average" value={parseFloat((good-bad)/(good+neutral+bad)).toFixed(4)} />
+        <StatisticLine text="positive" value={parseFloat(good/(good+neutral+bad)*100).toFixed(2)+"%"} />
+      </tbody>
     </table>
   )
 }
