ba3af713a00d8565b95b7848bb955b74982b798a
EternalAzure <kall.miika@gmail.com>
2021-08-24 14:35:30 +0300

    PUT joka vaatii vain likes ja hakee itse loput tiedot tietokannasta

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { request, response } = require('express')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -19,6 +20,30 @@ blogsRouter.post('/', async (request, response) => {
   const savedBlog = await blog.save()
   response.json(savedBlog.toJSON())
 
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  Blog.findById(request.params.id, function(err, blog) {
+    if(err) next(err)
+
+    const newBlog = {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: request.body.likes
+    }
+
+    Blog.findByIdAndUpdate(request.params.id, newBlog, {new:true})
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON())
+    })
+    .catch(error => next(error))
+  })  
 })
 
 module.exports = blogsRouter
