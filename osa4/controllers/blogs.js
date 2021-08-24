98016e99d81995a7ac5130660bec7ee20cc5d82e
EternalAzure <kall.miika@gmail.com>
2021-08-24 14:43:28 +0300

    4.13-4.14

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { request, response } = require('express')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -19,6 +20,28 @@ blogsRouter.post('/', async (request, response) => {
   const savedBlog = await blog.save()
   response.json(savedBlog.toJSON())
 
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const newBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, newBlog, {new:true})
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 module.exports = blogsRouter
