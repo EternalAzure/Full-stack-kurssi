cff51973950ee5fc5001477d926e6ffc60264352
EternalAzure <kall.miika@gmail.com>
2021-12-16 15:07:08 +0200

    6.19

--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -1,24 +1,34 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
-  }
+  }  
 
-  const showNotification = useSelector(state => state.show)
-  if (showNotification === false) {
-    return null
+  if (!props.notification) {
+    return (null)
   }
-
   return (
     <div style={style}>
-      {notification}
+      {props.notification}
     </div>
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  if (state.show === false) {
+    return {}
+  }
+  return {
+    notification: state.notification
+  }
+}
+
+
+export default connect(
+  mapStateToProps,
+  null
+)(Notification)
